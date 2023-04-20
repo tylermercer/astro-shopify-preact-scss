bcccab417ea8e1369760a08f60527129524603e0
Tyler Mercer <acct@tylermercer.net>
2023-04-19 20:01:21 -0600

    Add astro/preact and nanostores/preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,21 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    svelte(),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), svelte(), preact()]
 });
